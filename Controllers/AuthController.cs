c257112a8ceee0e53d6a9b7f6b47bdb5e364ba45
SapnaSingh-dev <sapnareena.2002@gmail.com>
2025-10-01 08:44:41 +0530

    First Commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using FirstProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login([FromBody]Loginreq req)
+        {
+            var res = new Loginres();
+
+            if(req.Email=="admin@gmail.com" && req.Password == "Admin@123")
+            {
+                res.Statuscode = 1;
+                res.msg = " Login Success";
+            }
+            else
+            {
+                res.Statuscode = -1;
+                res.msg = " Login failed";
+            }
+            return Json(res);
+        }
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AuthModal.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstProject.Models
+{
+  
+    public class Loginreq
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class Loginres
+    {
+        public int Statuscode { get; set; }
+        public string msg { get; set; }
+    }
+    public class Users
+    {
+        [Required(ErrorMessage ="Name is required.")]
+        public string Full_Name { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage ="Your Email is Invalid")]
+        public string  Email{ get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [Phone(ErrorMessage ="Your Phone number is Invalid")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$",
+        ErrorMessage = "Password must be 6-20 characters, include uppercase, lowercase, number, and special character.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Plese Confirm your password")]
+        [Compare("Password", ErrorMessage ="This is not matching password")]
+
+        public string Confirm_Password { get; set; }
+        [Required(ErrorMessage = "Gender is required.")]
+        public string Gender { get; set; }
+
+    }
+}
